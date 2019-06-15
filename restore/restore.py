d9a3bd0e314838bcb813eda7485c0fbef3ab93eb
ナッティ <hello@nutty.cc>
2019-06-14 20:28:10 -0700

    Extract backup archive to `/minecraft`

--- restore/restore.py
+++ restore/restore.py
@@ -1,5 +1,6 @@
 import os
 import pathlib
+import tarfile
 
 from flask import Flask, render_template, request
 from werkzeug.utils import secure_filename
@@ -10,16 +11,20 @@ app = Flask(
     static_folder = "./dist/static",
     template_folder = "./dist",
 )
-app.config["UPLOAD_FOLDER"] = "./temp"
+
+app.config["UPLOAD_FOLDER"] = "/tmp"
+app.config["MINECRAFT_FOLDER"] = "/"
 
 
 @app.route("/")
 def serve_vue_app():
+    """Serve the Vue application."""
     return render_template("index.html")
 
 
 @app.route("/upload", methods=["POST"])
 def upload_backup():
+    """Extract backup gzipped tarball to the Minecraft server directory."""
     file = request.files.get("file")
     if not file:
         return "No file attached...", 400
@@ -28,17 +33,20 @@ def upload_backup():
     elif not file.filename.endswith("tar.gz"):
         return "Not a gzipped tarball...", 400
     else:
+        # Save backup to disk.
         filename = secure_filename(file.filename)
         filepath = os.path.join(app.config["UPLOAD_FOLDER"], filename)
         pathlib.Path(app.config["UPLOAD_FOLDER"]).mkdir(parents=True, exist_ok=True)
         file.save(filepath)
-        restore_backup(filepath)
+        # Extract the backup to server directory.
+        tarball = tarfile.open(filepath, "r:gz")
+        tarball.extractall(app.config["MINECRAFT_FOLDER"])
         return "Success!"
 
 
-def restore_backup(filepath):
-    pass
-
-
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    if not os.path.isdir(app.config["MINECRAFT_FOLDER"]):
+        print("Minecraft server files not found. Starting restoration server.")
+        app.run(host="0.0.0.0")
+    else:
+        print("Minecraft server files found! Exiting init container.")
