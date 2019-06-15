a2928a7bce4916cd2ccb32b728f21202662612e5
ナッティ <hello@nutty.cc>
2019-06-14 23:40:46 -0700

    Backup the contents of `/minecraft` instead of dir

--- backup/backup.sh
+++ backup/backup.sh
@@ -2,6 +2,6 @@
 
 TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 printf "[$(date +"%Y-%m-%d %r")] Creating Minecraft Server Backup... "
-tar -cpvzf /backups/backup_${TIMESTAMP}.tar.gz /minecraft &> /dev/null
+tar -cpvzf /backups/backup_${TIMESTAMP}.tar.gz /minecraft/* &> /dev/null
 echo "complete"
 
